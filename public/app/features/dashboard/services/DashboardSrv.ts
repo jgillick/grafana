dfabf3440d409aa6f82eb3fbf920584cc6324f42
Elfo404 <me@giordanoricci.com>
2022-02-18 17:23:14 +0000

    DashboardSrv: add saveDashboard method

--- public/app/features/dashboard/services/DashboardSrv.ts
+++ public/app/features/dashboard/services/DashboardSrv.ts
@@ -1,11 +1,18 @@
 import { appEvents } from 'app/core/app_events';
 import { DashboardModel } from '../state/DashboardModel';
 import { removePanel } from '../utils/panel';
-import { DashboardMeta } from 'app/types';
+import { DashboardDataDTO, DashboardMeta } from 'app/types';
 import { getBackendSrv } from 'app/core/services/backend_srv';
 import { saveDashboard } from 'app/features/manage-dashboards/state/actions';
 import { RemovePanelEvent } from '../../../types/events';
+import { BackendSrvRequest } from '@grafana/runtime';
 
+interface SaveDashboardOptions {
+  dashboard: DashboardDataDTO;
+  message?: string;
+  folderId?: number;
+  overwrite?: boolean;
+}
 export class DashboardSrv {
   dashboard?: DashboardModel;
 
@@ -40,6 +47,18 @@ export class DashboardSrv {
     return saveDashboard({
       dashboard: parsedJson,
       folderId: this.dashboard?.meta.folderId || parsedJson.folderId,
+    });
+  }
+
+  saveDashboard(
+    data: SaveDashboardOptions,
+    requestOptions?: Pick<BackendSrvRequest, 'showErrorAlert' | 'showSuccessAlert'>
+  ) {
+    return getBackendSrv().request({
+      url: '/api/dashboards/db/',
+      method: 'POST',
+      data,
+      ...requestOptions,
     });
   }
 
